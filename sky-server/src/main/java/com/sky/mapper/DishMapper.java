52ed2113e6c0cf3a1e919333257d823482daa3d9
ming <2629175467@qq.com>
2026-02-06 13:52:23 +0800

    删除和修改菜品

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -7,31 +7,74 @@ import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
-
-    /**
-     * 根据分类id查询菜品数量
-     * @param categoryId
-     * @return
-     */
-    @Select("select count(id) from dish where category_id = #{categoryId}")
-    Integer countByCategoryId(Long categoryId);
-	
 	
 	/**
-     * 新增菜品数据
-     * @param dish
-     */
-	@AutoFill(value = OperationType.INSERT)
+	 * 根据分类id查询菜品数量
+	 * @param categoryId
+	 * @return
+	 */
+	Integer countByCategoryId(@Param("categoryId") Long categoryId);
+	
+	/**
+	 * 添加菜品
+	 * @param dish
+	 */
+	@AutoFill(OperationType.INSERT)
 	void insert(Dish dish);
 	
 	/**
-     * 菜品查询
-     * @param dishPageQueryDTO
-     * @return
-     */
+	 * 菜品分页查询
+	 * @param dishPageQueryDTO
+	 * @return
+	 */
 	Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+	
+	/**
+	 * 获取菜品
+	 * @param id
+	 * @return
+	 */
+	Dish getById(Long id);
+	
+	/**
+	 * 删除菜品表中的菜品数据
+	 * @param id
+	 */
+	void deleteById(Long id);
+	
+	/**
+	 * 修改菜品基本信息
+	 * @param dish
+	 */
+	@AutoFill(OperationType.UPDATE)
+	void update(Dish dish);
+	
+	/**
+	 * 根据分类id查询菜品
+	 * @param dish
+	 * @return
+	 */
+	List<Dish> list(Dish dish);
+	
+	/**
+	 * 根据套餐id查询菜品
+	 * @param id
+	 * @return
+	 */
+	List<Dish> getBySetmealId(Long id);
+	
+	/**
+	 * 根据条件统计菜品数量
+	 * @param map
+	 * @return
+	 */
+	Integer countByMap(Map map);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,24 +1,16 @@
-package com.sky.service;/**
- * @author Hjm
- * @date 2026/2/5 19:06
- * @motto 不经一番寒彻骨 怎得梅花扑鼻香
- * @description
- */
+package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
-/**
- * @author Hjm
- * @date 2026/2/5 19:06
- * @description
- */
+import java.util.List;
 
-public interface DishService
-{
+public interface DishService {
 	/**
-	 * 新增菜品和对应的口味
+	 * 新增菜品
 	 * @param dishDTO
 	 */
 	void saveWithFlavor(DishDTO dishDTO);
@@ -29,4 +21,44 @@ public interface DishService
 	 * @return
 	 */
 	PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+	
+	/**
+	 * 菜品批量删除
+	 * @param ids
+	 */
+	void deleteBatch(List<Long> ids);
+	
+	/**
+	 * 根据id查询菜品
+	 * @param id
+	 * @return
+	 */
+	DishVO getByIdWithFlavor(Long id);
+	
+	/**
+	 * 修改菜品
+	 * @param dishDTO
+	 */
+	void updateWithFlavor(DishDTO dishDTO);
+	
+	/**
+	 * 根据分类id查询菜品
+	 * @param categoryId
+	 * @return
+	 */
+	List<Dish> list(Long categoryId);
+	
+	/**
+	 * 条件查询菜品和口味
+	 * @param dish
+	 * @return
+	 */
+	List<DishVO> listWithFlavor(Dish dish);
+	
+	/**
+	 * 菜品起售停售
+	 * @param status
+	 * @param id
+	 */
+	void startOrStop(Integer status, Long id);
 }
